4d016641c4ac96236ed5953fed8da3aaea023b32
Damir Vodenicarevic <damipator@gmail.com>
2020-10-19 01:25:11 +0200

    working setup with file uplaod and upload trafic shaping

--- wrapper/entrypoint.sh
+++ wrapper/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 # setup network simulation
-DL_KBPS="$1"
-DL_MS="$2"
-UL_KBPS="$2"
-UL_MS="$3"
-shift 4
-echo "$DL_KBPS $DL_MS $UL_KBPS $UL_MS"
+UL_KBITPS="$1"
+UL_MS="$2"
+shift 2
+
+# upload
+tc qdisc add dev eth0 root handle 1:0 tbf rate "${UL_KBITPS}kbit" buffer "$((UL_KBITPS>1600 ? UL_KBITPS : 1600))" limit 10000
+tc qdisc add dev eth0 parent 1:0 handle 10: netem delay "${UL_MS}ms"  # loss 0.03%
 
 # run program
 "$@"
