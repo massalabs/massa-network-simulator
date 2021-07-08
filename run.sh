8961f4af0d57db14a0006d9a76a185efd4d96a57
Damir Vodenicarevic <damipator@gmail.com>
2021-07-08 16:28:17 +0000

    rename scenarii to scenarios

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ cd ../massa-network && \
     cargo build --target x86_64-unknown-linux-musl && \
     cp target/x86_64-unknown-linux-musl/debug/massa_network ../network-simulator/wrapper/massa_network && \
     cd - && \
-    python3 main.py
+    python3.7 main.py
 
