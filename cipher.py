81e7e442112e0192adb1f5d066c3b6bf78cf1e25
sydhds <sydhds@massa.net>
2022-10-20 15:56:07 +0200

    Resoter to cryptography lib

--- cipher.py
+++ cipher.py
@@ -3,15 +3,15 @@ import os, binascii
 from passlib.hash import pbkdf2_sha256
 import varint
 import hashlib
-from Crypto.Cipher import AES
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
 def encrypt(data: str):
     salt = binascii.unhexlify('646464646464646464646464')
     salt_encoded = base64.b64decode(salt)
     key = hashlib.pbkdf2_hmac('sha256', b'test', salt_encoded, 10000, 32)
     nonce = binascii.unhexlify('656565656565656565656565')
-    cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
-    encrypted = cipher.encrypt(data.encode("utf-8"))
+    cipher = AESGCM(key)
+    encrypted = cipher.encrypt(nonce, data.encode("utf-8"), None)
     result = bytearray()
     result.extend(varint.encode(0))
     result.extend(salt)
